c28281ccb1bf9b21907c4bdc81a7d9f7454722af
zone100054 <zone100054@gmail.com>
2019-04-18 18:58:32 +0200

    xd

--- Main-Core/src/main/java/net/bapehc/core/cmd/ItemshopCommands.java
+++ Main-Core/src/main/java/net/bapehc/core/cmd/ItemshopCommands.java
@@ -1,8 +1,13 @@
 package net.bapehc.core.cmd;
 
+import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
 
 public class ItemshopCommands implements CommandExecutor {
 
-
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        return false;
+    }
 }
