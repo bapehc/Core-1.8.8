b425f62183d7c7757e7bd8731381639ebbe22930
Piechuuu <RoDaSekYT@wp.pl>
2019-04-18 19:23:42 +0200

    d

--- Piechuuu-Core/src/main/java/core/JavaCore.java
+++ Piechuuu-Core/src/main/java/core/JavaCore.java
@@ -1,13 +1,25 @@
 package core;
 
+import core.cmd.ItemshopCommand;
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class JavaCore extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        registerCommand();
+        registerListeners();
 
+    }
+
+    private void registerCommand() {
+        getCommand("is").setExecutor(new ItemshopCommand());
+    }
+
+    private void registerListeners() {
+        PluginManager pm = Bukkit.getPluginManager();
     }
 
     @Override
