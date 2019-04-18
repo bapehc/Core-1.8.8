8c3912440e9382562a370b8f470518353a818f4c
zone100054 <zone100054@gmail.com>
2019-04-18 18:38:24 +0200

    klasa itemshop

--- Main-Core/src/main/java/net/bapehc/core/CorePlugin.java
+++ Main-Core/src/main/java/net/bapehc/core/CorePlugin.java
@@ -4,14 +4,15 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public final class CorePlugin extends JavaPlugin {
 
-    @Override
     public void onEnable() {
-        // Plugin startup logic
+        registerCommands();
+        registerListeners();
 
     }
 
-    @Override
+    private void registerCommands() {
+    }
+
     public void onDisable() {
-        // Plugin shutdown logic
     }
 }
---
+++ Main-Core/src/main/java/net/bapehc/core/cmd/ItemshopCommands.java
@@ -0,0 +1,4 @@
+package net.bapehc.core.cmd;
+
+public class ItemshopCommands {
+}
